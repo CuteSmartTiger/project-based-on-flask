598ceb430e5f0aa3714a6e6e5c202118d29a8e9c
MAX-liuhu <max_liuhu@163.com>
2019-01-10 13:48:00 +0800

    Update demo01.py

--- 01_first_sqlalchemy/demo01.py
+++ 01_first_sqlalchemy/demo01.py
@@ -18,10 +18,10 @@ conn = engine.connect()
 result = conn.execute('select 1')
 print(result.fetchone())
 
-class Person(object):
-    name = 'xx'
-    age = 18
-    country ='xx'
+# class Person(object):
+#     name = 'xx'
+#     age = 18
+#     country ='xx'
 
 # Person类 -> 数据库中的一张表
 # Person类中的属性  -> 数据库中一张表字段
